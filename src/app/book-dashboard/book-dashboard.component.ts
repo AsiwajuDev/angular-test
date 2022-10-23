fec028baa0b250064ad86ccbdbd99b2504cc54d6
asiwaju <olanrewajuranda@gmail.com>
2022-10-23 22:55:29 +0100

    Added API Service

--- src/app/book-dashboard/book-dashboard.component.ts
+++ src/app/book-dashboard/book-dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-dashboard',
   templateUrl: './book-dashboard.component.html',
-  styleUrls: ['./book-dashboard.component.css']
+  styleUrls: ['./book-dashboard.component.css'],
 })
 export class BookDashboardComponent implements OnInit {
+  formValue!: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      author: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(3)]],
+      category: ['', [Validators.required, Validators.minLength(3)]],
+    });
   }
-
 }
